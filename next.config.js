09d54e132966bf14300fb317ab73704980e01b0c
danielevans254 <danielevans254@yahoo.com>
2023-12-18 09:59:59 +0800

    Add verified domain for image uploading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.discordapp.com', 'cdn.discord.com', "uploadthing.com", "utfs.io"],
+  },
+}
 
 module.exports = nextConfig
