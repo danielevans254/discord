e59da3e91b85cf12ecf586219b302c729554847c
danielevans254 <danielevans254@yahoo.com>
2023-12-20 15:29:09 +0800

    Add Modal Store

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,21 +1,25 @@
+'use client';
 import { Plus } from "lucide-react";
 import ActionTooltip from "../action-tooltip";
+import { ModalStore } from "@/hooks/use-modal-store";
 
 const NavigationAction = () => {
+  const { onOpen } = ModalStore();
+
   return (
     <div>
       <ActionTooltip
         label="Create a Server"
         side="right"
         align="end">
-        <button className="group flex items-center">
+        <button className="group flex items-center" onClick={() => onOpen("createServer")}>
           <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
             <Plus className="group-hover:text-white transition text-emerald-500" size={24} />
-
           </div>
         </button>
       </ActionTooltip>
-    </div >);
+    </div>
+  );
 }
 
 export default NavigationAction;
