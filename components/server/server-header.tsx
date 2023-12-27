2c5e9f2c72494422f316391c9a04935f7350f0a1
danielevans254 <danielevans254@yahoo.com>
2023-12-27 09:37:25 +0800

    Add all necessary menu items

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,7 +2,7 @@
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { ChevronDown, LogOut, Settings, Trash2, Tv, User, UserCog, UserPlus, Users } from "lucide-react";
+import { Bell, CalendarDays, ChevronDown, FolderPlus, LogOut, Pencil, PlusCircle, Settings, ShieldAlert, Sticker, Trash2, Tv, User, UserCog, UserPlus, Users } from "lucide-react";
 import Image from "next/image";
 import { ScrollArea } from "../ui/scroll-area";
 import { useEffect, useState } from "react";
@@ -11,6 +11,8 @@ import { MdPreview } from "react-icons/md";
 import { TbBan } from "react-icons/tb";
 import { PiFileMagnifyingGlassFill } from "react-icons/pi";
 import { BsEmojiSmile } from "react-icons/bs";
+import { Separator } from "../ui/separator";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles,
@@ -37,122 +39,75 @@ const ServerHeader = ({
     return null;
   }
 
-  const renderServerActions = () => (
-    <div className="flex flex-col px-3 py-2 space-y-2">
-      <span className="font-semibold">Server Actions</span>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/30 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        Server Settings
-        <Settings className="ml-auto" size={16} />
-      </DropdownMenuItem>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        Invite People
-        <UserPlus className="ml-auto" size={16} />
-      </DropdownMenuItem>
-      <DropdownMenuItem className="w-full text-left hover:bg-red-700/90 dark:hover:bg-red-700/80 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        Leave Server
-        <LogOut className="ml-auto" size={16} />
-      </DropdownMenuItem>
-      <DropdownMenuItem className="w-full text-left hover:bg-red-700/90 dark:hover:bg-red-700/80 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        Delete Server
-        <Trash2 className="ml-auto" size={16} />
-      </DropdownMenuItem>
-    </div>
-  );
+  // TODO: Add functionality
 
-  const renderServerMembers = () => (
+  const renderUserActions = () => (
     <div className="flex flex-col px-3 py-2 space-y-2">
-      <span className="font-semibold">Server Members</span>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        View Members
-        <Users className="ml-auto" size={16} />
-      </DropdownMenuItem>
-      {isModerator && (
-        <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-          Manage Members
-          <UserCog className="ml-auto" size={16} />
+      <DropdownMenuContent className="w-56 text-xs font-medium text-black space-y-[2px]">
+        <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          Invite People
+          <UserPlus className="ml-auto" size={16} />
         </DropdownMenuItem>
-      )}
-    </div>
-  );
+        {isModerator && (
+          <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+            Server Settings
+            <Settings className="ml-auto" size={16} />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+            Create Channel
+            <PlusCircle className="ml-auto" size={16} />
+          </DropdownMenuItem>
+        )}
 
-  const renderServerChannels = () => (
-    <div className="flex flex-col px-3 py-2 space-y-2">
-      <span className="font-semibold">Server Channels</span>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        View Channels
-        <Tv className="ml-auto" size={16} />
-      </DropdownMenuItem>
-      {isAdmin && (
-        <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-          Manage Channels
-          <MdOutlineManageHistory className="ml-auto" size={18} />
+        {isModerator && (
+          <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+            Create Category
+            <FolderPlus className="ml-auto" size={16} />
+          </DropdownMenuItem>
+        )}
+
+        {isModerator && (
+          <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+            Create Event
+            <CalendarDays className="ml-auto" size={16} />
+          </DropdownMenuItem>
+        )}
+
+        <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+          App Directory
+          <Sticker className="ml-auto" size={16} />
         </DropdownMenuItem>
-      )}
-    </div>
-  );
-
-  const renderServerRoles = () => (
-    <div className="flex flex-col px-3 py-2 space-y-2">
-      <span className="font-semibold">Server Roles</span>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        View Roles
-        <MdPreview className="ml-auto" size={18} />
-      </DropdownMenuItem>
-      {isAdmin && (
-        <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-          Manage Roles
-          <MdOutlineManageSearch className="ml-auto" size={18} />
+        <Separator />
+        <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+          Show All Channels
+          <Tv className="ml-auto" size={16} />
         </DropdownMenuItem>
-      )}
-    </div>
-  );
-
-  const renderServerBans = () => (
-    <div className="flex flex-col px-3 py-2 space-y-2">
-      <span className="font-semibold">Server Bans</span>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        View Bans
-        <PiFileMagnifyingGlassFill className="ml-auto" size={18} />
-      </DropdownMenuItem>
-      {isAdmin && (
-        <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-          Manage Bans
-          <TbBan className="ml-auto" size={18} />
+        <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+          Notification Settings
+          <Bell className="ml-auto" size={16} />
         </DropdownMenuItem>
-      )}
-    </div>
-  );
-
-  const renderServerIntegrations = () => (
-    <div className="flex flex-col px-3 py-2 space-y-2">
-      <span className="font-semibold">Server Integrations</span>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        View Integrations
-      </DropdownMenuItem>
-      {isAdmin && (
-        <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-          Manage Integrations
+        <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+          Privacy Settings
+          <ShieldAlert className="ml-auto" size={16} />
         </DropdownMenuItem>
-      )}
-    </div>
-  );
-
-  const renderServerEmojis = () => (
-    <div className="flex flex-col px-3 py-2 space-y-2">
-      <span className="font-semibold">Server Emojis</span>
-      <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-        View Emojis
-        <BsEmojiSmile className="ml-auto" size={18} />
-
-      </DropdownMenuItem>
-      {isModerator && (
-        <DropdownMenuItem className="w-full text-left hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition rounded-md px-2 py-1 text-neutral-500 dark:text-neutral-300">
-          Manage Emojis
-          <MdOutlineManageHistory className="ml-auto" size={18} />
+        <Separator />
+        <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+          Edit Server Profile
+          <Pencil className="ml-auto" size={16} />
         </DropdownMenuItem>
-      )}
+        <DropdownMenuItem className="text-gray-300 px-3 py-2 text-sm cursor-pointer">
+          Hide Muted Channels
+          <MdCheckBoxOutlineBlank className="ml-auto" size={16} />
+        </DropdownMenuItem>
+        <Separator />
+        <DropdownMenuItem className="text-red-600 dark:text-red-500 px-3 py-2 text-sm cursor-pointer">
+          Leave Server
+          <LogOut className="ml-auto" size={16} />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </div>
-
   );
 
 
@@ -165,28 +120,8 @@ const ServerHeader = ({
           <ChevronDown className="ml-auto" size={24} />
         </button>
       </DropdownMenuTrigger>
-
-      <DropdownMenuContent className="">
-        <ScrollArea className="h-72 w-48 rounded-md">
-          <div className="flex items-center px-3 py-2 space-x-2">
-            <div className="flex-shrink-0 w-12 h-12 rounded-full overflow-hidden border-2 ">
-              <Image src={server.imageUrl} alt="Server Image" className="flex flex-col items-center" width={48} height={48} />
-            </div>
-            <div className="flex flex-col justify-end">
-              <span className="text-sm text-neutral-500 dark:text-neutral-300">{server.members.length} Members</span>
-              <span className="text-sm text-neutral-500 dark:text-neutral-300">{adminCount} Admins</span>
-              <span className="text-sm text-neutral-500 dark:text-neutral-300">{moderatorCount} Moderators</span>
-            </div>
-          </div>
-          {renderServerActions()}
-          {renderServerMembers()}
-          {renderServerChannels()}
-          {renderServerRoles()}
-          {renderServerBans()}
-          {/* {renderServerIntegrations()} */}
-          {renderServerEmojis()}
-        </ScrollArea>
-      </DropdownMenuContent>
+      {renderUserActions()}
+      {/* {isAdmin && renderAdminActions()} */}
     </DropdownMenu>
   );
 }
